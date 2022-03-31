e828ed3a252ab1d5d5ad06d6aed06f94cd2c9931
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-03-31 16:43:04 -0500

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,10 +3,10 @@ from translator import englishToFrench, frenchToEnglish
 
 class TestMyModule(unittest.TestCase):
     def test_englishToFrench(self):
-        self.assertEqual(englishToFrench(""),"")
+        self.assertEqual(englishToFrench(" ")," ")
         self.assertEqual(englishToFrench("Hello"),"Bonjour")
     def test_frenchToEnglish(self):
-        self.assertEqual(frenchToEnglish(""),"")
+        self.assertEqual(frenchToEnglish(" ")," ")
         self.assertEqual(frenchToEnglish("Bonjour"),"Hello")
 
 if __name__=='__main__':
